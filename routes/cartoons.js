32eba38ed2fbb177e043c2f6418a792762c207ec
Edgers <a.b.23042000@gmail.com>
2021-11-17 19:00:24 +0200

    Never gonna make you cry

--- routes/cartoons.js
+++ routes/cartoons.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const fs = require('fs')
 const router = express.Router()
 
 router.get(`/:subpage`, (req, res) => {
-    res.render('cartoons/' + req.params.subpage, {video: req.params.subpage})
+    let path = 'assets/videos/' + req.params.subpage + '.mp4'
+    fs.access(path, fs.F_OK, (err) => {
+        if (err) {
+            res.render('404')
+        }
+        res.render('cartoons/cartoon_tmplt', {video: req.params.subpage})
+    })
 })
 
 module.exports = router
--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const fs = require("fs");
 const router = express.Router()
 
 router.get(`/:page`, (req, res) => {
-    res.render(req.params.page)
+    let path = 'views/' + req.params.page + '.ejs'
+    fs.access(path, fs.F_OK, (err) => {
+        if (err) {
+            res.render('404')
+        }
+        res.render(req.params.page)
+    })
 })
 
 module.exports = router
